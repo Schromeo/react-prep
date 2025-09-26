c74c2c77a0e81576869499bdeb3b28d1be94490e
schromeo <spouringrainc@gmail.com>
2025-09-26 15:57:22 -0700

    first change of App.js

--- react-prep/src/App.js
+++ react-prep/src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello React!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World! This is my first React App!
         </p>
         <a
           className="App-link"
@@ -15,11 +16,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React from ReactJS.org
         </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
